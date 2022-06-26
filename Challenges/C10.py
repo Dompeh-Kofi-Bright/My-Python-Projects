e6316c167da1a1ac62002bf2c7ef53f840879f85
Dompeh Kofi Bright <bright2002dompeh@gmail.com>
2022-06-26 08:07:04 +0100

    Initial commit

---
+++ Challenges/C10.py
@@ -0,0 +1,18 @@
+from random import*
+
+prompt = "> "
+options = ["rock", "paper", "scissors"]
+state = "continue"
+while(state == "continue"):
+    answer = choice(options)
+    User = input("Rock, Paper, Scissors!!(Type Quit to end game))\n %s" %(prompt))
+    if(answer == User):
+        print("You're so worthy!!")
+    elif(User == "Quit" or User =="quit"):
+        state = "Stop"
+        print("\nGAME OVER\n")
+    else:
+        print("Looser!!")
+
+        
+
---
+++ Loops.py
@@ -0,0 +1,16 @@
+grades = []
+j =1
+i = 0
+while(j<=5):
+    grade=input("Enter Grade\n > ")
+    grades.append(grade)
+    j = j + 1
+
+print("Your Grades are: ")
+
+while(i<=4):
+    print(grades[i])
+    i = i + 1
+
+print("Done")
+    
